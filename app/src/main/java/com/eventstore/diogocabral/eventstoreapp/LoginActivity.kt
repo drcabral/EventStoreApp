1b87e66cb626fdcda054c8ef5703713a061c3f53
Diogo Cabral <drkbral@gmail.com>
2019-01-31 02:43:18 -0200

    Adding basic firebase configuration.

--- app/src/main/java/com/eventstore/diogocabral/eventstoreapp/LoginActivity.kt
+++ app/src/main/java/com/eventstore/diogocabral/eventstoreapp/LoginActivity.kt
@@ -2,11 +2,34 @@ package com.eventstore.diogocabral.eventstoreapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.loginButton
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            signIn(emailEditText.text.toString(), passwordEditText.text.toString())
+        }
+    }
+
+    fun signIn(username: String, password: String) {
+        auth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Toast.makeText(baseContext, "Logged with ${user?.email}", Toast.LENGTH_SHORT ).show()
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 }
